4fb69b658b44de25b2a7452fdd488c71aab5202a
1vnazarov <nazaroffvana.boeing@yandex.ru>
2024-09-17 21:54:41 +0300

    Все переверстано, валидация

--- js/footer.js
+++ js/footer.js
@@ -1,7 +1,9 @@
-window.onload = () => {
+window.addEventListener("load", function() {
     const footer = document.createElement("footer")
+    footer.classList.add("text-center", "py-4", "bg-dark", "text-white")
+    footer.style.bottom = 0
     const footerText = document.createElement("p")
     footerText.innerText = "Проспект Авиаконструкторов, 28"
     footer.appendChild(footerText)
     document.body.appendChild(footer)
-}
+})
