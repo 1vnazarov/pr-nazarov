2c92935d75dd74cfbd1d25dff6e910026f296c82
1vnazarov <nazaroffvana.boeing@yandex.ru>
2024-09-19 19:26:50 +0300

    Лишние файлы удалены

--- js/onAuthSubmit.js
+++
@@ -1,7 +0,0 @@
-const onAuthSubmit = async (e) => {
-    e.preventDefault()
-    await fetch("https://pr-nazarov.сделай.site/authorize_user.php", {
-        method: "POST",
-        body: JSON.stringify({email: document.getElementById("email").value, password: document.getElementById("password").value})
-    })
-}
